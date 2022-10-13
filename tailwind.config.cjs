1f3ec21369c1b27d0fbd55333335a9d2f64046d4
jessicaidro <jessicaidro@gmail.com>
2022-10-13 11:57:58 -0300

    Config Storybook part2 e Importando tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,24 @@ module.exports = {
         './src/**/*.tsx'
     ],
     theme: {
+        fontSize:{
+            xs: 14,
+            sm: 16,
+            md: 18,
+            lg: 20,
+            xl: 24,
+            '2xl': 32,
+        },
+        colors:{
+            'grey-900' : '#121214',
+            'grey-800' : '#202024',
+            'grey-400' : '#7C7C8A',
+            'grey-200' : '#C4C4CC',
+            'grey-100' : '#E1E1EC',
+
+            'cyan-500': '#81D8F7',
+            'cyan-300': '#98E1FB'
+        },
         extend: {
             fontFamily:{
                 sans: 'Inter, sans-serif'
